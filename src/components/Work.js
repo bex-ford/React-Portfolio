36fedc427c1f81b8805ae057ba12615dc500c170
bex-ford <becky782@hotmail.co.uk>
2023-03-04 09:42:26 +0000

    included bootstrap

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,27 +2,28 @@ import React from 'react'
 
 function Work() {
   return (
-   <div className="card-deck">
-    <div className="card col-sm-12 col-md-4 col-lg-4">
-      <a href="https://github.com/bex-ford/module-1"> 
-      <img src="/" class="card-img-top custom-card-img" alt="..."/> </a>    
-      <div className=''>
-        <h5 className="card-title">Module-1</h5>
-        <p className="card-text">
-          <p>
-              <a href="https://github.com/bex-ford/project-1" target="_blank" rel="noopener"
-                ><strong><u>Github Project Module-1</u></strong></a
-              >
-              </p>
-            <p>
-              <a href="https://bex-ford.github.io/module-1/" target="_blank" rel="noopener"
-                ><strong><u>JustVeggie</u></strong></a
-              > 
-            </p> 
+ <div className="card-deck">
+<div className="card col-sm-12 col-md-4 col-lg-4">
+  <a href="https://github.com/bex-ford/module-1"> 
+  <img src="/src/assets/justveggie.png" class="card-img-top custom-card-img" alt="JustVeggie"/> </a>    
+  <div className=''>
+    <h5 className="card-title">JustVeggie</h5>
+    <p className="card-text">
+        <p>
+          <a href="https://bex-ford.github.io/module-1/" target="_blank" rel="noreferrer"
+            ><strong><u>JustVeggie Application</u></strong></a
+          > 
+        </p> 
+        <p>
+          <a href="https://github.com/bex-ford/project-1"  rel="noreferrer" target="_blank"
+            ><strong><u>GitHub</u></strong></a
+          >
           </p>
-      </div>
-      </div>
-      </div>
-  )
+      </p>
+  </div>
+  </div>
+  </div>
+)
 }
 export default Work
+
