b1fad3a7b366634ce6a1d61bd35a4ccf4453dd97
bex-ford <becky782@hotmail.co.uk>
2023-03-04 08:47:05 +0000

    added links to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>About</div> 
-  )
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      {/* <a class="navbar-brand" href="/">Home</a> */}
+  {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button> */}
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">
+              Home <span class="sr-only"></span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/#about">
+              About
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/#skills">
+              Skills
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="/#work">
+              Work
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="/#contact">
+              Contact
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="/src/assets/BF CV.pdf">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-export default Navbar
+export default Navbar;
